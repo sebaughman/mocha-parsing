0ffc1282fd312997761da5eb1d7aa9ebb184b565
Craig Parker <fegmur3@gmail.com>
2018-06-02 14:05:36 -0600

    This is what I have so far. We can choose whatever word we want instead of Testing

--- fileToExecute.js
+++ fileToExecute.js
@@ -1,15 +1,48 @@
 
 var exec = require('child_process').exec;
 
+// let string;
+
 exec('mocha', ['test.js'], function(error, out, err) {
     if (error !== null) {
         console.log(error);
     }
+    const objToSend = {
+        q: 'Q1',
+        passed: false, 
+        tests: {}
+    };
     console.log(out);
-    console.log(err);
-    return out
+    const selectedArray = out.substring(out.indexOf("^[a-zA-Z0-9]+$"), out.indexOf('\n\n', out.indexOf('Testing'))).trim().split('\n');
+    console.log(selectedArray);
+    selectedArray.forEach((string, i) => {
+        if (i === 0) {
+            objToSend.q = string.trim();
+        }
+        else if (!/\d/.test(string)) {
+            const id = `T${i}`;
+            objToSend.tests[id] = true;
+        }
+        else {
+            const id = `T${i}`;
+            objToSend.tests[id] = false;
+        }
+    });
+
+    if (!Object.values(objToSend.tests).includes(false)) {
+        objToSend.passed = true;
+    }
+
+    console.log(objToSend);
+    const jsonObject = JSON.stringify(objToSend);
+    console.log(jsonObject);
+
+    return jsonObject;
 });
 
+// console.log(string);
+
+// var String=string.substring(string.lastIndexOf("Testing")+1,string.lastIndexOf("passing"));
 
 // const { spawn } = require('child_process');
 
